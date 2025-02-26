7dfc3a48daa7746d18fa8fca145fe9680ca4091b
191204taianh <anhnt.22bi13028@usth.edu.vn>
2025-02-26 08:44:50 +0700

    Khong biet la xong hay chua

---
+++ Frontend/script.js
@@ -0,0 +1,6 @@
+<script>
+  function scrollToSearch() {
+    document.getElementById("search-bar").scrollIntoView({ behavior: "smooth" });
+    document.getElementById("search-bar").querySelector("input").focus(); // Focus on the search bar
+  }
+</script>
