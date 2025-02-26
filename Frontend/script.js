0f74a58fcc729325af28d0a0d47dc52f8aba0f51
HSw109 <letruong7a21@gmail.com>
2025-02-27 02:15:27 +0700

    Upload frontend

--- Frontend/script.js
+++
@@ -1,6 +0,0 @@
-<script>
-  function scrollToSearch() {
-    document.getElementById("search-bar").scrollIntoView({ behavior: "smooth" });
-    document.getElementById("search-bar").querySelector("input").focus(); // Focus on the search bar
-  }
-</script>
